2ddd5db25c759251c7d0d130f9d3a8c05ef11e43
marwan1108 <53006783+marwan1108@users.noreply.github.com>
2023-05-13 20:11:58 +0300

    Add files via upload

---
+++ Python projects/Dice Rolling Stimulator/Dice Rolling Stimulator.py
@@ -0,0 +1,82 @@
+import random
+
+
+while True:
+    number = random.randint(1, 6)
+    if number == 1 :
+        print("--------")
+        print("|      |")
+        print("|   0  |")
+        print("|      |")
+        print("--------")
+    if number == 2 :
+        print("--------")
+        print("|      |")
+        print("| 0  0 |")
+        print("|      |")
+        print("--------")
+    if number == 3 :
+        print("--------")
+        print("|   0  |")
+        print("|   0  |")
+        print("|   0  |")
+        print("--------")
+    if number == 4 :
+        print("--------")
+        print("| 0  0 |")
+        print("|      |")
+        print("| 0  0 |")
+        print("--------")
+    if number == 5 :
+        print("--------")
+        print("| 0  0 |")
+        print("|   0  |")
+        print("| 0  0 |")
+        print("--------")
+    if number == 6 :
+        print("--------")
+        print("| 0  0 |")
+        print("| 0  0 |")
+        print("| 0  0 |")
+        print("--------")  
+    print(number)
+    choice = input("Roll again? (y/s) ")
+    if choice == 's':
+     break
+    if number == 1 :
+        print("--------")
+        print("|      |")
+        print("|   0  |")
+        print("|      |")
+        print("--------")
+    if number == 2 :
+        print("--------")
+        print("|      |")
+        print("| 0  0 |")
+        print("|      |")
+        print("--------")
+    if number == 3 :
+        print("--------")
+        print("|   0  |")
+        print("|   0  |")
+        print("|   0  |")
+        print("--------")
+    if number == 4 :
+        print("--------")
+        print("| 0  0 |")
+        print("|      |")
+        print("| 0  0 |")
+        print("--------")
+    if number == 5 :
+        print("--------")
+        print("| 0  0 |")
+        print("|   0  |")
+        print("| 0  0 |")
+        print("--------")
+    if number == 6 :
+        print("--------")
+        print("| 0  0 |")
+        print("| 0  0 |")
+        print("| 0  0 |")
+        print("--------")  
+    print(number)
